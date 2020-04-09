1147b195a3d7abb9a8b046319c6a9d66f5ecc31b
uedayusuke <uedayusuke1107@gmail.com>
2020-04-09 14:19:58 +0900

    [Finish]6-8

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$("document").ready(function(){
+	$(".theTarget").skippr();
+});
+
+$(".theTarget").skippr({
+	transition : "fade",
+	speed : 1000,
+	easing : "easeOutQuart",
+	navType : "block",
+	childrenElementType : "div",
+	arrows : true,
+	autoPlay : true,
+	autoPlayDuration : 5000,
+	keyboardOnAlways : true,
+	hidePrevious : false
+});
